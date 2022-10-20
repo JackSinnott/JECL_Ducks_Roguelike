25ac4f390136cb098347bb38e04ff9e75709b97c
unknown <c00248444@itcarlow.ie>
2022-10-20 13:35:58 +0100

    JDRL-24 complete

--- Roguelike/Enemy.cpp
+++ Roguelike/Enemy.cpp
@@ -2,8 +2,11 @@
 
 Enemy::Enemy(EnemyType t_type, float t_posX, float t_posY) : m_type(t_type)
 {
-	m_enemy.setPosition(sf::Vector2f(t_posX, t_posY));
-	m_enemy.setSize(sf::Vector2f(100, 100));
+	int xCoord = (int)t_posX - (int)t_posX % G_CELL_SIZE + G_CELL_SIZE / 2;
+	int yCoord = (int)t_posY - (int)t_posY % G_CELL_SIZE + G_CELL_SIZE / 2;
+	m_enemy.setOrigin(G_CELL_SIZE / 2, G_CELL_SIZE / 2);
+	m_enemy.setPosition(sf::Vector2f(xCoord, yCoord));
+	m_enemy.setSize(sf::Vector2f(G_CELL_SIZE, G_CELL_SIZE));
 	m_enemy.setFillColor(sf::Color::Red);
 }
 
