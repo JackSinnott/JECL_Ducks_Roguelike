566bca0917aee21e0e6151546c2c32792c77ff37
leo <leochen1020@hotmail.com>
2022-10-20 14:00:06 +0100

    Minor changes

--- Roguelike/Item.cpp
+++ Roguelike/Item.cpp
@@ -2,17 +2,17 @@
 
 Item::Item(std::string t_itemImageName, sf::Vector2f t_position, int t_rarity)
 {
+
 	if (!m_itemTexture.loadFromFile("./ASSETS/IMAGES/" + t_itemImageName))
 	{
 		std::cout << "This image file could not be loaded: " << t_itemImageName << std::endl;
 	}
-	else
-	{
-		m_itemSprite.setTexture(m_itemTexture);
-		m_itemSprite.setPosition(t_position);
-		m_itemSprite.setOrigin(m_itemSprite.getTextureRect().width / 2, m_itemSprite.getTextureRect().height / 2);
-		m_rarity = t_rarity;
-	}
+
+	m_itemSprite.setPosition(t_position);
+	m_itemSprite.setTexture(m_itemTexture);
+	m_itemSprite.setOrigin(m_itemSprite.getTextureRect().width / 2, m_itemSprite.getTextureRect().height / 2);
+	m_rarity = t_rarity;
+	
 
 }
 
