d9f1bdab0d79576454e604137b4bfa7d2b4494d1
Leo <c00250593@itcarlow.ie>
2022-11-30 15:38:32 +0000

    More work on the inventory

--- Roguelike/AbstractPotion.h
+++ Roguelike/AbstractPotion.h
@@ -6,7 +6,9 @@ class AbstractPotion : public AbstractItem
 public:
 	~AbstractPotion() {};
 	void SetType() { m_itemType = ItemType::Potion; };
-
+	virtual int UseEffect() = 0;
+	void SetPotionType(Potions t_potionType) { m_potionType = t_potionType; };
+	Potions GetPotionType() { return m_potionType; };
 protected:
 	Potions m_potionType;
 };
