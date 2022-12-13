e55314c75512c4fca87500e857b39f33d32bb184
Caroline Percy <lineypercy@me.com>
2022-12-13 15:48:12 +0000

    Updated some combat doxygen

--- Roguelike/CombatSystem.cpp
+++ Roguelike/CombatSystem.cpp
@@ -7,15 +7,7 @@ int* CombatSystem::playerHealth = nullptr;
 /// 
 /// </summary>
 /// <param name="t_health"></param>
-void CombatSystem::SetPlayerHealth(int* t_health)
-{
-	playerHealth = t_health;
-}
-
-/// <summary>
-/// 
-/// </summary>
-/// <param name="t_health"></param>
+/// <param name="t_xp"></param>
 void CombatSystem::PushBackEnemyStats(int* t_health, int t_xp)
 {
 	enemiesStats.push_back(std::make_pair(t_health, t_xp));
@@ -74,10 +66,11 @@ int CombatSystem::HurtEnemy(int t_indexOfEnemy, int damage)
 }
 
 /// <summary>
-/// Returns a random number between 1 and 20
+/// Returns a random number between 1 and 20 (inclusive), adds them together if the amount rolled is more than 1, then returns the result.
 /// </summary>
-/// <param name="amountRolled">The amount of times that dice should be rolled</param>
-/// <returns>the resulting roll</returns>
+/// <param name="amountRolled">The amount of times that dice should be rolled.</param>
+/// <returns>the resulting roll of all the dice,
+/// ie. if the dice is rolled twice and got 10 and 16, 26 is returned.</returns>
 int CombatSystem::RollD20(int amountRolled)
 {
 	int roll = 0;
@@ -90,6 +83,12 @@ int CombatSystem::RollD20(int amountRolled)
 	return roll;
 }
 
+/// <summary>
+/// Generates a number between 1 and 8 (inclusive), adds them together if the amount rolled is more than 1, then returns the result.
+/// </summary>
+/// <param name="amountRolled">The amount of times the dice is rolled.</param>
+/// <returns>The total number of the rolls,
+/// ie. if the dice is rolled twice and got 2 and 3, 5 is returned.</returns>
 int CombatSystem::RollD8(int amountRolled)
 {
 	int roll = 0;
--- Roguelike/CombatSystem.h
+++ Roguelike/CombatSystem.h
@@ -5,18 +5,20 @@
 class CombatSystem
 {
 private:
-	// static Ref to enemies' healths
+
+	/// <summary>
+	/// Reference to all the enemies' healths,
+	/// and how much xp they would give the player when they're defeated.
+	/// </summary>
 	static std::vector<std::pair<int*, int>> enemiesStats;
 
-	// static ref to player's health
-	static int* playerHealth;
+	static int* playerHealth; // Reference to the player's health.
 
 	static int RollD20(int amountRolled = 1);
 
 public:
 	static int RollD8(int amountRolled = 1);
 
-	static void SetPlayerHealth(int* t_health);
 	static void PushBackEnemyStats(int* t_health, int t_xp);
 
 	static bool BattleEquation(int defendersArmour, int attackersLevel = 0, int t_modifiers = 0);
