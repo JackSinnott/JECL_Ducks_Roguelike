3231f1db7f7638ee278baa0e6b3ae73cca651b6f
Caroline Percy <lineypercy@me.com>
2022-12-15 16:41:01 +0000

    Up-to-date doxygen

--- Roguelike/Player.h
+++ Roguelike/Player.h
@@ -10,40 +10,53 @@
 #include "CombatSystem.h"
 
 /// <summary>
-/// Class the player directly controls.
+/// The class the user directly controls.
 /// </summary>
 
 class Player
 {
 private:
-	//Field of view of the player
+	///The field of view of the player.
 	sf::View m_playerView;
 
-	// Variables
-
-	//sf::Sprite m_playerBody; 
-
+	///Whether the Player is able to move right this turn.
 	bool m_freeToMoveRight = true;
+
+	///Whether the Player is able to move left this turn.
 	bool m_freeToMoveLeft = true;
+
+	///Whether the Player is able to move up this turn.
 	bool m_freeToMoveUp = true;
+
+	///Whether the Player is able to move down this turn.
 	bool m_freeToMoveDown = true;
 	
-
+	///The Player's position last turn.
 	sf::Vector2i m_previousPos;
 
-	int m_x, m_y;
+	///The row the player is in on the Grid.
+	int row;
 
-	int row, col;
+	///The column the player is in on the Grid.
+	int col;
+
 	/// Appearance of player.
-	/// For now it will be a rectangle shape, until a spritesheet can be sorted.
 	sf::Sprite m_playerBody;
 
-	/// Reference to the texture of the player.
+	///Reference to the texture of the player.
 	std::shared_ptr<sf::Texture> m_playerTexture;
+
+	/// <summary>
+	/// The appearance of the player on the spritesheet, depending on the armours they can have.
+	/// </summary>
 	static std::unordered_map<Armours, sf::IntRect> m_rects;
+
+	/// <summary>
+	/// The appearance of the player on the spritesheet, if they are wearing no armour.
+	/// </summary>
 	static sf::IntRect m_noArmourRect;
 
-	//A basic inventory
+	///The Player's inventory. This is where their items are stored.
 	Inventory m_playerInventory;
 
 	/// <summary>
@@ -51,13 +64,20 @@ private:
 	/// </summary>
 	sf::Keyboard::Key m_pressingButton = sf::Keyboard::Unknown;
 
-	int m_strength = 10; // The strength of the player, used for attacking
-	int m_armourClass = 2; // The defense of the player, used for defending
-	int m_health = 20; // The health of the player. if this reaches 0, the game is over.
-	int level = 1; // The players level. as level increases, so does the health, strength, and defense.
-	int xp = 0; // the players xp. When this reaches a certain threshold, level goes up by 1. XP is gotten by defeating enemies.
+	///The strength of the player, used for attacking.
+	int m_strength = 10; 
+	///The defense of the player, used for defending
+	int m_armourClass = 2;
+	///The health of the player. if this reaches 0, the game is over.
+	int m_health = 20; 
+	///The players level. as level increases, so does the health, strength, and defense.
+	int level = 1; 
+	///the players xp. When this reaches a certain threshold, level goes up by 1. XP is gotten by defeating enemies.
+	int xp = 0; 
 
-	int m_maxHealth = 20; //Current maximum health the player can have
+	///The current maximum health the player can have.
+	int m_maxHealth = 20;
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
 	// Methods / Functions
 
 	void UpdateArmourLook();
@@ -68,6 +88,7 @@ private:
 	int GetDamageModifier();
 
 public:
+
 	Player();
 	Player(int t_row, int t_col);
 
@@ -94,16 +115,52 @@ public:
 	sf::Vector2f GetPosition();
 	void setGridPosition(int row, int col);
 	
+	/// <summary>
+	/// Returns whether the Player can move up.
+	/// </summary>
+	/// <returns>Can the player move up?</returns>
 	inline bool canWeMoveUp() { return m_freeToMoveUp; }
+
+	/// <summary>
+	/// Changes whether the Player can move up or not.
+	/// </summary>
+	/// <param name="t_switch">Change to if the player can move up.</param>
 	inline void setMovementBoolUp(bool t_switch) { m_freeToMoveUp = t_switch; }
 
+	/// <summary>
+	/// Changes whether the Player can move down or not.
+	/// </summary>
+	/// <param name="t_switch">Change to if the player can move down.</param>
 	inline void setMovementBoolDown(bool t_switch) { m_freeToMoveDown = t_switch; }
+
+	/// <summary>
+	/// Returns whether the Player can move down.
+	/// </summary>
+	/// <returns>Can the player move down?</returns>
 	inline bool canWeMoveDown() { return m_freeToMoveDown; }
 
+	/// <summary>
+	/// Changes whether the Player can move left or not.
+	/// </summary>
+	/// <param name="t_switch">Change to if the player can move left.</param>
 	inline void setMovementBoolLeft(bool t_switch) { m_freeToMoveLeft = t_switch; }
+
+	/// <summary>
+	/// Returns whether the Player can move left.
+	/// </summary>
+	/// <returns>Can the player move left?</returns>
 	inline bool canWeMoveLeft() { return m_freeToMoveLeft; }
 
+	/// <summary>
+	/// Changes whether the Player can move right or not.
+	/// </summary>
+	/// <param name="t_switch">Change to if the player can move right.</param>
 	inline void setMovementBoolRight(bool t_switch) { m_freeToMoveRight = t_switch;	}
+
+	/// <summary>
+	/// Returns whether the Player can move right.
+	/// </summary>
+	/// <returns>Can the player move right?</returns>
 	inline bool canWeMoveRight() { return m_freeToMoveRight; }
 
 	// Inventory Methods
--- Roguelike/Room.h
+++ Roguelike/Room.h
@@ -7,6 +7,12 @@
 #include <array>
 #include "Tile.h"
 #include <iostream>
+
+/// <summary>
+/// Class that handles Tile objects, and organises them together to look and act coherant.
+/// Also is used to seperate itself from other Rooms.
+/// </summary>
+
 class Room
 {
 public:
@@ -20,54 +26,56 @@ public:
 	void setUpTiles(TileType t_type, int row, int col);
 
 	/// <summary>
-	/// 
+	/// Returns the Room's ID.
 	/// </summary>
-	/// <returns></returns>
+	/// <returns>The Room's ID.</returns>
 	inline int getID() { return id; }
 
 	/// <summary>
-	/// 
+	/// Gets the start position of the Room in relation to the whole Grid. 
 	/// </summary>
-	/// <returns></returns>
+	/// <returns>The position of the Room.</returns>
 	inline sf::Vector2f getPosition() { return m_pos; }
 
 	/// <summary>
-	/// 
+	/// Get all the positions of the Tiles that are #VERTICAL_WALL,
+	/// #HORIZONTAL_WALL, or #INSIDE_WALL.
 	/// </summary>
-	/// <returns></returns>
+	/// <returns>All the positions of the walls.</returns>
 	inline std::vector<sf::Vector2i> getWalls() { return m_walls; };
 
 	void setGridPosition();
 
 	/// <summary>
-	/// 
+	///Gets the row and column of this Room occupies in the Grid.
 	/// </summary>
-	/// <returns></returns>
+	/// <returns>The row and column of this Room.</returns>
 	inline sf::Vector2i getGridPosition() { return sf::Vector2i(row, col); }
 
 private:
-	///
+	///The position of the Room.
 	sf::Vector2f m_pos;
 
-	///
+	///The shared pointer to the texture of the Tile objects.
 	std::shared_ptr<sf::Texture> m_pTexture;
 
-	///
+	///The tiles themselves, laid out in a 2D array.
 	std::array<std::array<Tile, G_MAP_COLS>,G_MAP_ROWS> m_tiles;
 
-	///
+	///All the positions of the Tiles that are #VERTICAL_WALL,
+	/// #HORIZONTAL_WALL, or #INSIDE_WALL.
 	std::vector<sf::Vector2i> m_walls;
 
-	///
+	///The row the Room is in on the Grid - All rooms are in a 3x3 layout together.
 	int row;
 
-	///
+	///The column the Room is in on the Grid - All rooms are in a 3x3 layout together.
 	int col;
 
-	///
+	///an ID for the Room - used for identification, like what room Player entered.
 	int id;
 
-	///
+	///Whether the Room has been visited by the Player, and should be drawn.
 	bool m_revealed;
 };
 
